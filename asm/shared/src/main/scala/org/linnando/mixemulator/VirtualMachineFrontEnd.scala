a513460d3930345d0036507e7cd44972ac36e3a1
Ruslan Batdalov <rbatdalov@google.com>
2022-12-10 13:05:14 +0100

    Extract UI-independent logic from VirtualMachineService in webapp to VirtualMachineFrontEnd in the library.

---
+++ asm/shared/src/main/scala/org/linnando/mixemulator/VirtualMachineFrontEnd.scala
@@ -0,0 +1,222 @@
+package org.linnando.mixemulator
+
+import org.linnando.mixemulator.asm.{MixAssembler, MixDisassembler}
+import org.linnando.mixemulator.vm.io.{BlockDevice, Device, LineDevice}
+import org.linnando.mixemulator.vm.io.data.IOWord
+import org.linnando.mixemulator.vm.io.file.{FileBlockIODevice, FileCardPunch, FileCardReader, FileDiskUnit, FileLineInputDevice, FileLineOutputDevice, FileLinePrinter, FilePaperTape, FileTapeUnit}
+import org.linnando.mixemulator.vm.{TrackingVirtualMachine, VirtualMachine, VirtualMachineState, VirtualMachineStateJs, binary, decimal}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class VirtualMachineFrontEnd(machine: VirtualMachine,
+                             lines: IndexedSeq[String],
+                             symbols: IndexedSeq[(Option[Short], Option[Int])],
+                             disassembler: MixDisassembler) {
+  private val addressSymbols: Vector[Int] =
+    symbols.indices.foldLeft(Vector.fill(VirtualMachine.MEMORY_SIZE)(0)) { (s, index) =>
+      val address = symbols(index)._1
+      if (address.isEmpty) s
+      else s.updated(address.get, index)
+    }
+
+  def currentState: VirtualMachineState = machine.currentState
+
+  def canMoveForward: Boolean = machine.canMoveForward
+
+  def stepForward(): Future[Unit] = machine.stepForward()
+
+  def runForward(): Future[Unit] = machine.runForward()
+
+  def canMoveBack: Boolean = machine match {
+    case m: TrackingVirtualMachine => m.canMoveBack
+    case _ => false
+  }
+
+  def stepBack(): Unit = machine match {
+    case m: TrackingVirtualMachine => m.stepBack()
+    case _ => throw new Error
+  }
+
+  def runBack(): Unit = machine match {
+    case m: TrackingVirtualMachine => m.runBack()
+    case _ => throw new Error
+  }
+
+  def symbolsLength: Int = symbols.length
+
+  def symbolIndices: Range = symbols.indices
+
+  def programCounterIndex: Int = addressSymbols(currentState.getProgramCounter)
+
+  def toggleBreakpointAt(index: Int): Unit = addressAt(index).foreach(machine.toggleBreakpoint)
+
+  def addressAt(index: Int): Option[Short] = symbols(index)._1
+
+  def breakpointAt(index: Int): Boolean = addressAt(index).exists(machine.breakpointAt)
+
+  def cellContent(index: Int): Option[IOWord] = addressAt(index).map(currentState.get)
+
+  def lineNumberAt(index: Int): Option[Int] = symbols(index)._2
+
+  def lineAt(index: Int): Option[String] = {
+    val (address, lineNumber) = symbols(index)
+    lineNumber.map(ln =>
+      if (address.exists(machine.isModified)) {
+        val cellContent = machine.currentState.get(address.get)
+        disassembler.disassembleLine(cellContent)
+      } else lines(ln)
+    )
+  }
+
+  def lineIsModifiedAt(index: Int): Boolean = addressAt(index).exists(machine.isModified)
+
+  def blockDeviceData(deviceNum: Int): Future[IndexedSeq[IOWord]] = device(deviceNum) match {
+    case d: BlockDevice => d.data
+    case _ => throw new Error
+  }
+
+  def blockDeviceDataJs(deviceNum: Int): js.Promise[js.Array[IOWord]] =
+    blockDeviceData(deviceNum).map(_.toJSArray).toJSPromise
+
+  private def device(deviceNum: Int): Device = currentState.getDevice(deviceNum)
+
+  def lineDeviceData(deviceNum: Int): Future[IndexedSeq[String]] = device(deviceNum) match {
+    case d: LineDevice => d.data
+    case _ => throw new Error
+  }
+}
+
+object VirtualMachineFrontEnd {
+  def createBinaryTracking(lines: Seq[String]): VirtualMachineFrontEnd = {
+    val builder = binary.createVirtualMachineBuilder()
+      .withDevices(createDevices())
+    val assembly = MixAssembler.translateTracking(builder, lines)
+    new VirtualMachineFrontEnd(assembly._1, lines.toVector, assembly._2.toVector, new MixDisassembler(binary))
+  }
+
+  def createBinaryNonTracking(lines: Seq[String]): VirtualMachineFrontEnd = {
+    val builder = binary.createVirtualMachineBuilder()
+      .withDevices(createDevices())
+    val assembly = MixAssembler.translateNonTracking(builder, lines)
+    new VirtualMachineFrontEnd(assembly._1, lines.toVector, assembly._2.toVector, new MixDisassembler(binary))
+  }
+
+  def createDecimalTracking(lines: Seq[String]): VirtualMachineFrontEnd = {
+    val builder = decimal.createVirtualMachineBuilder()
+      .withDevices(createDevices())
+    val assembly = MixAssembler.translateTracking(builder, lines)
+    new VirtualMachineFrontEnd(assembly._1, lines.toVector, assembly._2.toVector, new MixDisassembler(decimal))
+  }
+
+  def createDecimalNonTracking(lines: Seq[String]): VirtualMachineFrontEnd = {
+    val builder = decimal.createVirtualMachineBuilder()
+      .withDevices(createDevices())
+    val assembly = MixAssembler.translateNonTracking(builder, lines)
+    new VirtualMachineFrontEnd(assembly._1, lines.toVector, assembly._2.toVector, new MixDisassembler(decimal))
+  }
+
+  def goBinaryTracking(): Future[VirtualMachineFrontEnd] = {
+    val devices = createDevices()
+    binary.goTracking(devices, goDevice) map { machine =>
+      new VirtualMachineFrontEnd(
+        machine,
+        Vector.empty,
+        Vector.tabulate(VirtualMachine.MEMORY_SIZE)(i => (Some(i.toShort), None)),
+        new MixDisassembler(binary)
+      )
+    }
+  }
+
+  def goBinaryNonTracking(): Future[VirtualMachineFrontEnd] = {
+    val devices = createDevices()
+    binary.goNonTracking(devices, goDevice) map { machine =>
+      new VirtualMachineFrontEnd(
+        machine,
+        Vector.empty,
+        Vector.tabulate(VirtualMachine.MEMORY_SIZE)(i => (Some(i.toShort), None)),
+        new MixDisassembler(binary)
+      )
+    }
+  }
+
+  def goDecimalTracking(): Future[VirtualMachineFrontEnd] = {
+    val devices = createDevices()
+    decimal.goTracking(devices, goDevice) map { machine =>
+      new VirtualMachineFrontEnd(
+        machine,
+        Vector.empty,
+        Vector.tabulate(VirtualMachine.MEMORY_SIZE)(i => (Some(i.toShort), None)),
+        new MixDisassembler(decimal)
+      )
+    }
+  }
+
+  def goDecimalNonTracking(): Future[VirtualMachineFrontEnd] = {
+    val devices = createDevices()
+    decimal.goNonTracking(devices, goDevice) map { machine =>
+      new VirtualMachineFrontEnd(
+        machine,
+        Vector.empty,
+        Vector.tabulate(VirtualMachine.MEMORY_SIZE)(i => (Some(i.toShort), None)),
+        new MixDisassembler(decimal)
+      )
+    }
+  }
+
+  private val goDevice = 16
+
+  private def createDevices(): Map[Int, Device] = Map(
+    0 -> FileTapeUnit.create("device0"),
+    1 -> FileTapeUnit.create("device1"),
+    2 -> FileTapeUnit.create("device2"),
+    3 -> FileTapeUnit.create("device3"),
+    4 -> FileTapeUnit.create("device4"),
+    5 -> FileTapeUnit.create("device5"),
+    6 -> FileTapeUnit.create("device6"),
+    7 -> FileTapeUnit.create("device7"),
+    8 -> FileDiskUnit.create("device8"),
+    9 -> FileDiskUnit.create("device9"),
+    10 -> FileDiskUnit.create("device10"),
+    11 -> FileDiskUnit.create("device11"),
+    12 -> FileDiskUnit.create("device12"),
+    13 -> FileDiskUnit.create("device13"),
+    14 -> FileDiskUnit.create("device14"),
+    15 -> FileDiskUnit.create("device15"),
+    16 -> FileCardReader.create("device16"),
+    17 -> FileCardPunch.create("device17"),
+    18 -> FileLinePrinter.create("device18"),
+    20 -> FilePaperTape.create("device20")
+  )
+
+  def getBlockDeviceData(deviceNum: Int): Future[IndexedSeq[IOWord]] = deviceNum match {
+    case i if i >= 0 && i < 16 => FileBlockIODevice.getCurrentData(s"device$i")
+    case _ => throw new Error
+  }
+
+  def saveBlockDevice(deviceNum: Int, data: Array[Byte]): Future[Unit] = {
+    val device = deviceNum match {
+      case i if i >= 0 && i < 8 => FileTapeUnit.create(s"device$i", data)
+      case i if i >= 8 && i < 16 => FileDiskUnit.create(s"device$i", data)
+      case _ => throw new Error
+    }
+    device.task.map(_ => ())
+  }
+
+  def getLineDeviceData(deviceNum: Int): Future[IndexedSeq[String]] = deviceNum match {
+    case 16 => FileLineInputDevice.getCurrentData("device16")
+    case 17 => FileLineOutputDevice.getCurrentData("device17")
+    case 18 => FileLineOutputDevice.getCurrentData("device18")
+    case 20 => FileLineInputDevice.getCurrentData("device20")
+    case _ => throw new Error
+  }
+
+  def saveLineDevice(deviceNum: Int, data: String): Future[Unit] = {
+    val device = deviceNum match {
+      case 16 => FileCardReader.create("device16", data)
+      case 20 => FilePaperTape.create("device20", data)
+      case _ => throw new Error
+    }
+    device.task.map(_ => ())
+  }
+}
--- webapp/src/main/scala/org/linnando/mixemulator/webapp/VirtualMachineService.scala
+++ webapp/src/main/scala/org/linnando/mixemulator/webapp/VirtualMachineService.scala
@@ -1,11 +1,9 @@
 package org.linnando.mixemulator.webapp
 
 import angulate2.std._
-import org.linnando.mixemulator.asm.{MixAssembler, MixDisassembler}
-import org.linnando.mixemulator.vm._
+import org.linnando.mixemulator.VirtualMachineFrontEnd
+import org.linnando.mixemulator.vm.VirtualMachineState
 import org.linnando.mixemulator.vm.io.data.IOWord
-import org.linnando.mixemulator.vm.io.file._
-import org.linnando.mixemulator.vm.io.{BlockDevice, Device, LineDevice}
 import rxjs.Subject
 
 import scala.collection.immutable.SortedMap
@@ -27,157 +25,96 @@ HELLO      ALF  HELLO
 
   val stateChange: Subject[Unit] = new Subject()
 
-  private var lines: Vector[String] = Vector.empty
-
-  private var _machine: Option[VirtualMachine] = None
-
-  private var _symbols: Vector[(Option[Short], Option[Int])] = Vector.empty
-
-  private var _addressSymbols: Vector[Int] = Vector.empty
-
-  private var disassembler: MixDisassembler = _
+  private var maybeMachine: Option[VirtualMachineFrontEnd] = None
 
   def assemble(): Future[Unit] = Future {
-    lines = text.split("\n").toVector
-    val processingModel = mode match {
-      case "binary" => binary
-      case "decimal" => decimal
-    }
-    val builder = processingModel.createVirtualMachineBuilder()
-      .withDevices(VirtualMachineService.createDevices())
-    val assembly =
-      if (tracking) MixAssembler.translateTracking(builder, lines)
-      else MixAssembler.translateNonTracking(builder, lines)
-    _machine = Some(assembly._1)
-    _symbols = assembly._2.toVector
-    _addressSymbols = _symbols.indices.foldLeft(Vector.fill(VirtualMachine.MEMORY_SIZE)(0)) { (s, index) =>
-      val address = _symbols(index)._1
-      if (address.isEmpty) s
-      else s.updated(address.get, index)
-    }
-    disassembler = new MixDisassembler(processingModel)
+    val lines = text.split("\n").toVector
+    maybeMachine = Some(
+      mode match {
+        case "binary" =>
+          if (tracking) VirtualMachineFrontEnd.createBinaryTracking(lines)
+          else VirtualMachineFrontEnd.createBinaryNonTracking(lines)
+        case "decimal" =>
+          if (tracking) VirtualMachineFrontEnd.createDecimalTracking(lines)
+          else VirtualMachineFrontEnd.createDecimalNonTracking(lines)
+      }
+    )
   }
 
   def go(): Future[Unit] = {
-    val devices = VirtualMachineService.createDevices()
-    val processingModel = mode match {
-      case "binary" => binary
-      case "decimal" => decimal
+    val eventualMachine: Future[VirtualMachineFrontEnd] = {
+      mode match {
+        case "binary" =>
+          if (tracking) VirtualMachineFrontEnd.goBinaryTracking()
+          else VirtualMachineFrontEnd.goBinaryNonTracking()
+        case "decimal" =>
+          if (tracking) VirtualMachineFrontEnd.goDecimalTracking()
+          else VirtualMachineFrontEnd.goDecimalNonTracking()
+      }
     }
-    val eventualMachine =
-      if (tracking) processingModel.goTracking(devices, VirtualMachineService.goDevice)
-      else processingModel.goNonTracking(devices, VirtualMachineService.goDevice)
     eventualMachine map { machine =>
-      _machine = Some(machine)
-      _symbols = Vector.tabulate(VirtualMachine.MEMORY_SIZE)(i => (Some(i.toShort), None))
-      _addressSymbols = Vector.range(0, VirtualMachine.MEMORY_SIZE)
-      disassembler = new MixDisassembler(processingModel)
+      maybeMachine = Some(machine)
     }
   }
 
   def switchOffMachine(): Unit = {
-    lines = Vector.empty
-    _machine = None
-    _symbols = Vector.empty
-    _addressSymbols = Vector.empty
+    maybeMachine = None
   }
 
-  def isActive: Boolean = _machine.isDefined
+  def isActive: Boolean = maybeMachine.isDefined
 
-  def canMoveForward: Boolean = _machine.exists(_.canMoveForward)
+  def canMoveForward: Boolean = maybeMachine.exists(_.canMoveForward)
 
-  def runForward(): Future[Unit] = _machine match {
-    case Some(m) =>
-      m.runForward().map(stateChange.next)
-    case None => throw new Error
-  }
+  def runForward(): Future[Unit] = maybeMachine.map(_.runForward().map(stateChange.next)).getOrElse(Future {})
 
-  def stepForward(): Future[Unit] = _machine match {
-    case Some(m) =>
-      m.stepForward().map(stateChange.next)
-    case None => throw new Error
-  }
+  def stepForward(): Future[Unit] = maybeMachine.map(_.stepForward().map(stateChange.next)).getOrElse(Future {})
 
-  def canMoveBack: Boolean = _machine.exists({
-    case m: TrackingVirtualMachine => m.canMoveBack
-    case _ => false
+  def canMoveBack: Boolean = maybeMachine.exists(_.canMoveBack)
+
+  def runBack(): Unit = maybeMachine.foreach(m => {
+    m.runBack()
+    stateChange.next(Unit)
   })
 
-  def runBack(): Unit = _machine match {
-    case Some(m: TrackingVirtualMachine) =>
-      m.runBack()
-      stateChange.next(Unit)
-    case _ => throw new Error
-  }
-
-  def stepBack(): Unit = _machine match {
-    case Some(m: TrackingVirtualMachine) =>
-      m.stepBack()
-      stateChange.next(Unit)
-    case _ => throw new Error
-  }
-
-  def machineState: Option[VirtualMachineState] = _machine.map(_.currentState)
-
-  def symbolsLength: Int = _symbols.length
-
-  def symbolIndices: Range = _symbols.indices
-
-  def programCounterIndex: Option[Int] = machineState.map(s => _addressSymbols(s.getProgramCounter))
-
-  def toggleBreakpointAt(index: Int): Unit = _machine match {
-    case Some(m) => addressAt(index).foreach(m.toggleBreakpoint)
-    case None => throw new Error
-  }
-
-  def addressAt(index: Int): Option[Short] = _symbols(index)._1
-
-  def breakpointAt(index: Int): Option[Boolean] = _machine.map(m => addressAt(index).exists(m.breakpointAt))
-
-  def cellContent(index: Int): Option[IOWord] = {
-    val address = addressAt(index)
-    machineState.flatMap(s => address.map(s.get))
-  }
-
-  def lineNumberAt(index: Int): Option[Int] = _symbols(index)._2
-
-  def lineAt(index: Int): Option[String] = _machine.flatMap(m => {
-    val (address, lineNumber) = _symbols(index)
-    lineNumber.map(ln =>
-      if (address.exists(m.isModified)) {
-        val cellContent = m.currentState.get(address.get)
-        disassembler.disassembleLine(cellContent)
-      } else lines(ln)
-    )
+  def stepBack(): Unit = maybeMachine.foreach(m => {
+    m.stepBack()
+    stateChange.next(Unit)
   })
 
-  def lineIsModifiedAt(index: Int): Option[Boolean] = _machine.map(m => {
-    val address = addressAt(index)
-    address.exists(m.isModified)
-  })
+  def machineState: Option[VirtualMachineState] = maybeMachine.map(_.currentState)
+
+  def symbolsLength: Int = maybeMachine.map(_.symbolsLength).getOrElse(0)
+
+  def symbolIndices: Range = maybeMachine.map(_.symbolIndices).getOrElse(0 until 0)
+
+  def programCounterIndex: Option[Int] = maybeMachine.map(_.programCounterIndex)
+
+  def toggleBreakpointAt(index: Int): Unit = maybeMachine.foreach(_.toggleBreakpointAt(index))
+
+  def addressAt(index: Int): Option[Short] = maybeMachine.flatMap(_.addressAt(index))
+
+  def breakpointAt(index: Int): Option[Boolean] = maybeMachine.map(_.breakpointAt(index))
+
+  def cellContent(index: Int): Option[IOWord] = maybeMachine.flatMap(_.cellContent(index))
+
+  def lineNumberAt(index: Int): Option[Int] = maybeMachine.flatMap(_.lineNumberAt(index))
+
+  def lineAt(index: Int): Option[String] = maybeMachine.flatMap(_.lineAt(index))
+
+  def lineIsModifiedAt(index: Int): Option[Boolean] = maybeMachine.map(_.lineIsModifiedAt(index))
 
   def deviceNumbers: Iterable[Int] = VirtualMachineService.deviceNames.keys
 
   def deviceName(deviceNum: Int): String = VirtualMachineService.deviceNames(deviceNum)
 
-  def blockDeviceData(deviceNum: Int): Future[IndexedSeq[IOWord]] = device(deviceNum) match {
-    case Some(d: BlockDevice) => d.data
-    case _ => VirtualMachineService.getBlockDeviceData(deviceNum)
-  }
+  def blockDeviceData(deviceNum: Int): Future[IndexedSeq[IOWord]] =
+    maybeMachine.map(_.blockDeviceData(deviceNum)).getOrElse(VirtualMachineFrontEnd.getBlockDeviceData(deviceNum))
 
-  private def device(deviceNum: Int): Option[Device] =
-    _machine.map(_.currentState.getDevice(deviceNum))
-
-  def lineDeviceData(deviceNum: Int): Future[IndexedSeq[String]] =    device(deviceNum) match {
-      case Some(d: LineDevice) => d.data
-      case _ => VirtualMachineService.getLineDeviceData(deviceNum)
-    }
-
+  def lineDeviceData(deviceNum: Int): Future[IndexedSeq[String]] =
+    maybeMachine.map(_.lineDeviceData(deviceNum)).getOrElse(VirtualMachineFrontEnd.getLineDeviceData(deviceNum))
 }
 
 object VirtualMachineService {
-  private val goDevice = 16
-
   private val deviceNames: Map[Int, String] = SortedMap(
     0 -> "Tape Unit 0",
     1 -> "Tape Unit 1",
@@ -201,57 +138,9 @@ object VirtualMachineService {
     20 -> "Paper Tape"
   )
 
-  private def createDevices(): Map[Int, Device] = Map(
-    0 -> FileTapeUnit.create("device0"),
-    1 -> FileTapeUnit.create("device1"),
-    2 -> FileTapeUnit.create("device2"),
-    3 -> FileTapeUnit.create("device3"),
-    4 -> FileTapeUnit.create("device4"),
-    5 -> FileTapeUnit.create("device5"),
-    6 -> FileTapeUnit.create("device6"),
-    7 -> FileTapeUnit.create("device7"),
-    8 -> FileDiskUnit.create("device8"),
-    9 -> FileDiskUnit.create("device9"),
-    10 -> FileDiskUnit.create("device10"),
-    11 -> FileDiskUnit.create("device11"),
-    12 -> FileDiskUnit.create("device12"),
-    13 -> FileDiskUnit.create("device13"),
-    14 -> FileDiskUnit.create("device14"),
-    15 -> FileDiskUnit.create("device15"),
-    16 -> FileCardReader.create("device16"),
-    17 -> FileCardPunch.create("device17"),
-    18 -> FileLinePrinter.create("device18"),
-    20 -> FilePaperTape.create("device20")
-  )
+  def saveBlockDevice(deviceNum: Int, data: Array[Byte]): Future[Unit] =
+    VirtualMachineFrontEnd.saveBlockDevice(deviceNum, data)
 
-  def getBlockDeviceData(deviceNum: Int): Future[IndexedSeq[IOWord]] = deviceNum match {
-    case i if i >= 0 && i < 16 => FileBlockIODevice.getCurrentData(s"device$i")
-    case _ => throw new Error
-  }
-
-  def saveBlockDevice(deviceNum: Int, data: Array[Byte]): Future[Unit] = {
-    val device = deviceNum match {
-      case i if i >= 0 && i < 8 => FileTapeUnit.create(s"device$i", data)
-      case i if i >= 8 && i < 16 => FileDiskUnit.create(s"device$i", data)
-      case _ => throw new Error
-    }
-    device.task.map(_ => ())
-  }
-
-  def getLineDeviceData(deviceNum: Int): Future[IndexedSeq[String]] = deviceNum match {
-    case 16 => FileLineInputDevice.getCurrentData("device16")
-    case 17 => FileLineOutputDevice.getCurrentData("device17")
-    case 18 => FileLineOutputDevice.getCurrentData("device18")
-    case 20 => FileLineInputDevice.getCurrentData("device20")
-    case _ => throw new Error
-  }
-
-  def saveLineDevice(deviceNum: Int, data: String): Future[Unit] = {
-    val device = deviceNum match {
-      case 16 => FileCardReader.create("device16", data)
-      case 20 => FilePaperTape.create("device20", data)
-      case _ => throw new Error
-    }
-    device.task.map(_ => ())
-  }
+  def saveLineDevice(deviceNum: Int, data: String): Future[Unit] =
+    VirtualMachineFrontEnd.saveLineDevice(deviceNum, data)
 }
